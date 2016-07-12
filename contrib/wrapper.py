3ba7cb1a9f7bf8e17aa69022b4b21431ed89789f
xenx <atvorozhkov@yandex.ru>
2016-07-12 23:29:29 +0300

    Fix 100% value

--- contrib/wrapper.py
+++ contrib/wrapper.py
@@ -27,7 +27,7 @@ def get_volume():
 
     if now_volume in range(0, 20): now_volume = ' '
     if now_volume in range(20, 60): now_volume = ' '
-    if now_volume in range(60, 100): now_volume = ' '
+    if now_volume in range(60, 101): now_volume = ' '
 
     return now_volume
 
@@ -39,11 +39,11 @@ def get_battery():
     if battery in range(0, 20): battery = ' '
     if battery in range(20, 60): battery = ' '
     if battery in range(60, 80): battery = ' '
-    if battery in range(80, 100): battery = ' '
+    if battery in range(80, 101): battery = ' '
 
     return battery
 
 if __name__ == '__main__':
     while True:
-        print_line(get_volume() + '  ' + get_battery())
+        print_line(str(get_volume()) + '  ' + str(get_battery()))
         sleep(1)
--- utils/web-searcher.sh
+++ utils/web-searcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL='https://duckduckgo.com/?q='
-QUERY=$(echo '' | dmenu -p "Search:" -fn "-xos4-terminus-medium-r-*-*-14-*")
+QUERY=$(echo '' | dmenu -p " " -fn "-xos4-terminus-medium-r-*-*-14-*")
 if [ -n "$QUERY" ]; then
   xdg-open "${URL}${QUERY}" 2> /dev/null
   exec i3-msg [class="^Firefox$"] focus
