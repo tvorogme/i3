0ee8f1b12e12c9b70d7dda53eb95d8cea0d4ad0a
xenx <atvorozhkov@yandex.ru>
2016-07-12 17:59:57 +0300

    Add helps for web search

--- utils/web-searcher.sh
+++ utils/web-searcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL='https://duckduckgo.com/?q='
-QUERY=$(echo '!g' | dmenu -p "Search " -fn '-xos4-terminus-medium-r-*-*-14-*')
+QUERY=$(cat /home/true/.config/i3/utils/helpers/search-words | dmenu -p "Search " -fn '-xos4-terminus-medium-r-*-*-14-*')
 if [ -n "$QUERY" ]; then
   xdg-open "${URL}${QUERY}" 2> /dev/null
   exec i3-msg [class="^Firefox$"] focus
