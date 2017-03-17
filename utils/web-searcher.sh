b909374296713149d4e447c9099074738ee432bc
Andrey Tvorozhkov <tvorog@tvorog.me>
2017-03-17 10:15:44 +0300

    Add new computer

--- utils/web-searcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-URL='https://duckduckgo.com/?q='
-QUERY=$(cat /home/true/.config/i3/utils/helpers/search-words | dmenu -p "Search " -fn '-xos4-terminus-medium-r-*-*-14-*')
-if [ -n "$QUERY" ]; then
-  xdg-open "${URL}${QUERY}" 2> /dev/null
-  exec i3-msg [class="^Firefox$"] focus
-fi
